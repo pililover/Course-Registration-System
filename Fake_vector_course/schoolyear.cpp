c06876fb46a697a0a305868ce5cc2ebb5b6f059f
pililover <nkanh22@clc.fitus.edu.vn>
2023-03-18 09:45:06 +0700

    [Fixed] Add school year/ Add class to school year
    Non test carefully

--- Fake_vector_course/schoolyear.cpp
+++ Fake_vector_course/schoolyear.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <iomanip>
-#include <fstream>
 #include <cstring>
 
 using namespace std;
@@ -29,126 +28,153 @@ struct Course
     Student *enrolled_student;
 };
 
-struct vectors
-{
-    Course *data;
-    int size;
-    int capacity;
-};
-
-
-
 struct Semester
 {
     int semester_id;
     string start_day;
     string end_day;
-    vectors *courses;
+};
+
+struct Classes
+{
+    string class_name;
+    Student *data_student; // dynamic
+    int student_num;       // size
+    int student_capacity;
 };
 
 struct SchoolYear
 {
     string year_name;
-    Semester semesters;
+    Classes *data_classes;
+    int class_num;
+    int class_capacity;
 };
 
-void init(vectors *&arr)
+void init_school_year(SchoolYear *&arr)
 {
-    // allocate memory for the struct and its data array
-    arr = new vectors;
-    arr->data = new Course[1];
-    arr->size = 0;
-    arr->capacity = 1;
+    arr = new SchoolYear;
+    arr->data_classes = new Classes[1];
+    arr->class_num = 0;
+    arr->class_capacity = 1;
 }
 
-void resize(vectors *&arr, int new_capacity)
+void resize_school_year(SchoolYear *&arr, int new_capacity)
 {
-    // allocate a new array with the desired capacity
-    Course *new_data = new Course[new_capacity];
-
-    // copy the old data to the new array
-    for (int i = 0; i < arr->size; i++)
+    Classes *new_data = new Classes[new_capacity];
+    for (int i = 0; i < arr->class_num; i++)
     {
-        new_data[i] = arr->data[i];
+        new_data[i] = arr->data_classes[i];
     }
-
-    // free the old array
-    delete[] arr->data;
-
-    // update the struct fields
-    arr->data = new_data;
-    arr->capacity = new_capacity;
+    delete[] arr->data_classes;
+    arr->data_classes = new_data;
+    arr->class_capacity = new_capacity;
 }
 
-void push_back(vectors *&arr, Course value)
+SchoolYear *create_school_year(string year_name)
 {
-    // if the array is full, double its capacity
-    if (arr->size == arr->capacity)
-    {
-        // int new_capacity = arr->capacity * 2;
-        int new_capacity = arr->capacity + 15;
-        resize(arr, new_capacity);
-    }
-    // add the new element at the end of the array
-    arr->data[arr->size] = value;
-    arr->size++;
-}
-
-void pop_back(vectors *&arr)
-{
-    // if the array is not empty, remove the last element
-    if (arr->size > 0)
-    {
-        arr->size--;
-
-        // if the array is less than 25% full, halve its capacity
-        if (arr->size * 4 <= arr->capacity)
-        {
-            int new_capacity = arr->capacity / 2;
-            if (new_capacity < 1)
-            {
-                new_capacity = 1;
-            }
-            resize(arr, new_capacity);
-        }
-    }
-}
-
-SchoolYear create_school_year(string year_name)
-{
-    SchoolYear schoolyear;
-    schoolyear.year_name = year_name;
-    // Initialize the courses dynamic array
-    init(schoolyear.semesters.courses);
+    SchoolYear *schoolyear = new SchoolYear;
+    schoolyear->year_name = year_name;
     return schoolyear;
 }
 
-void add_class_to_schoolyear(SchoolYear &school_year, string class_name)
+void push_back_class(SchoolYear *&arr, Classes *value)
 {
-    Semester def_semester = school_year.semesters;
-    for (int i = 0; i < def_semester.courses->size; i++)
+    if (arr->class_num == arr->class_capacity)
     {
-        Course course = def_semester.courses->data[i];
-        if (course.class_name == class_name)
-        {
-            cout << "Error: Class already exists in current semester." << endl;
-            return;
-        }
+        int new_capacity = arr->class_capacity + 1; // just add one class
+        resize_school_year(arr, new_capacity);
     }
-    Course new_course;
-    new_course.class_name = class_name;
-    push_back(def_semester.courses, new_course);
-    school_year.semesters = def_semester;
-    cout << "Class " << class_name << " added to SchoolYear " << school_year.year_name << "." << endl;
+    arr->data_classes[arr->class_num] = Classes(*value);
+    // move it, not the copy of it
+    arr->class_num++;
 }
+
+void init_class(Classes *&arr)
+{
+    arr = new Classes;
+    arr->data_student = new Student[1];
+    arr->student_num = 0;
+    arr->student_capacity = 1;
+}
+
+void resize_class(Classes *&arr, int new_capacity)
+{
+    Student *new_data = new Student[new_capacity];
+    for (int i = 0; i < arr->student_num; i++)
+    {
+        new_data[i] = arr->data_student[i];
+    }
+    delete[] arr->data_student;
+    arr->data_student = new_data;
+    arr->student_capacity = new_capacity;
+}
+
+void add_class_to_schoolyear(SchoolYear *&school_year, string classname)
+{
+    Classes *new_class = new Classes;
+    init_class(new_class);
+    new_class->class_name = classname;
+    push_back_class(school_year, new_class);
+    cout << "Class " << school_year->data_classes[school_year->class_num - 1].class_name << " added to SchoolYear " << school_year->year_name << "." << endl;
+}
+
+void push_back_student(Classes *&arr, Student value)
+{
+    if (arr->student_num == arr->student_capacity)
+    {
+        int new_capacity = arr->student_capacity + 15;
+        resize_class(arr, new_capacity);
+    }
+    arr->data_student[arr->student_num] = value; // add to the end
+    arr->student_num++;
+}
+
+void view_class_in_schoolyear(SchoolYear *school_year)
+{
+    for (int i = 0; i < school_year->class_num; i++)
+        cout << school_year->data_classes[i].class_name << endl;
+}
+
 int main()
 {
-    string year_name, class_name;
+    string year_name, classname;
+    int op;
     cout << "School year: ";
     cin >> year_name;
-    SchoolYear school_year = create_school_year(year_name);
-    cout << "Class name (add class to semester): ";
-    cin >> class_name;
-    add_class_to_schoolyear(school_year, class_name);
+    SchoolYear *school_year = create_school_year(year_name);
+    do
+    {
+        cout << "Option: ";
+        cin >> op;
+        switch (op)
+        {
+        case 0:
+        {
+            break;
+        }
+        case 1:
+        {
+            cout << "Class name (add class to schoolyear): ";
+            cin >> classname;
+            add_class_to_schoolyear(school_year, classname);
+            break;
+        }
+        case 2:
+        {
+            view_class_in_schoolyear(school_year);
+            break;
+        }
+        }
+    } while (op != 0);
+    for (int i = 0; i < school_year->class_num; i++)
+    {
+        delete[] school_year->data_classes[i].data_student;
+    }
+    for (int i = 0; i < school_year->class_num; i++)
+    {
+        delete[] school_year[i].data_classes;
+    }
+    delete[] school_year;
     return 0;
 }
