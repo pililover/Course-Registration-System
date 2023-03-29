b33ddcea50e515211a7de0e6b84b5a805521d288
pililover <nkanh22@clc.fitus.edu.vn>
2023-03-29 20:22:19 +0700

    View list of course

---
+++ Fake_vector_course/view_list_course.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <sstream>
+
+using namespace std;
+struct Score
+{
+    int student_id;
+    float score;
+};
+
+struct Student
+{
+    string student_num;
+    string studentID;
+    string firstname;
+    string lastname;
+    string gender;
+    string birthday;
+    string socialID;
+};
+
+struct Course
+{
+    string course_id;
+    string course_name;
+    string class_name;
+    string teacher_name;
+    int credits;
+    int max_students = 50;
+    string day_of_week;
+    string session;
+    Student *enrolled_student;
+    int size_student;
+    int capacity_student;
+    Score *scores;
+};
+
+struct Semester
+{
+    Course *data_course = nullptr;
+    string start_day;
+    string end_day;
+    int course_num = 0;
+    int capacity_course = 0;
+};
+
+struct Classes
+{
+    string class_name;
+    Student *data_student = nullptr; // dynamic
+    int student_num;                 // size
+    int student_capacity;
+};
+
+struct SchoolYear
+{
+    string year_name; // Ex:2022-2023???
+    Classes *data_classes;
+    Semester *semester_data;
+    int semester_num = 3;
+    int current_semester;
+    int class_num;
+    int class_capacity;
+};
+
+void recapacity_student_in_a_course(Course *&course_need_add_student_now, int new_capacity)
+{
+    Student *array_student_need_add_now = course_need_add_student_now->enrolled_student;
+    Student *new_array = new Student[new_capacity];
+    for (int i = 0; i < course_need_add_student_now->size_student; i++)
+        new_array[i] = array_student_need_add_now[i];
+    delete[] array_student_need_add_now;
+    course_need_add_student_now->enrolled_student = new_array;
+    course_need_add_student_now->capacity_student = new_capacity;
+}
+
+void push_back_student_to_course(Course *&course_need_add_student_now, Student &value)
+{
+    if (course_need_add_student_now->size_student == course_need_add_student_now->capacity_student)
+    {
+        int new_capacity = (course_need_add_student_now->capacity_student == 0) ? 1 : course_need_add_student_now->capacity_student + 15;
+        recapacity_student_in_a_course(course_need_add_student_now, new_capacity);
+    }
+    course_need_add_student_now->size_student++;
+    course_need_add_student_now->enrolled_student[course_need_add_student_now->size_student] = value;
+}
+
+void view_list_of_course(SchoolYear *&school_year)
+{
+    cout << "\t\tLIST OF COURSE IN THIS SEMESTER" << endl;
+    if (school_year[school_year->current_semester].semester_data->course_num == 0)
+    {
+        cout << "Course not found" << endl;
+        return;
+    }
+    for (int i = 0; i < school_year[school_year->current_semester].semester_data->course_num; i++)
+    {
+        cout << i + 1 << ". "
+             << school_year[school_year->current_semester].semester_data->data_course->course_id
+             << school_year[school_year->current_semester].semester_data->data_course->course_name << endl;
+    }
+}
