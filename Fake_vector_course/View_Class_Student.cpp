ed844128a3172287923b9356d62318b8dfe8e7ea
VTKKhoi <118881962+VTKKhoi@users.noreply.github.com>
2023-04-06 22:47:54 +0700

    alignment

--- Fake_vector_course/View_Class_Student.cpp
+++ Fake_vector_course/View_Class_Student.cpp
@@ -14,8 +14,9 @@ void view_class_in_schoolyear(SchoolYear *school_year) {
 		cout << school_year->data_classes[i].class_name << endl;
 }
 
-//Check school year
-/*	if(!school_year) {
+
+/*Check school year first	
+	if(!school_year) {
 		cout << "No school year available" << endl;
 		break;
 	}
@@ -44,14 +45,18 @@ void view_student_in_class(SchoolYear *school_year) {
 	if(school_year->data_classes[index].student_num == 0) {
 		cout<<"No student in class"<<endl; return;
 	}
-	cout<<"No "<<"ID "<<"First name "<<"Last name "<<"Gender "<<"Birthday "<<"Social ID"<<endl;
+	
+	cout<<setw(4)<<left<<"No"<<setw(12)<<left<<"ID"
+	<<setw(15)<<left<<"First name"<<setw(15)<<left<<"Last name"
+	<<setw(9)<<left<<"Gender"<<setw(14)<<left<<"Birthday"
+	<<"Social ID"<<endl;
 	for(int i=0; i<school_year->data_classes[index].student_num; i++) {
-		cout << school_year->data_classes[index].data_student[i].student_num<< " ";
-		cout << school_year->data_classes[index].data_student[i].studentID << " ";
-        cout << school_year->data_classes[index].data_student[i].firstname << " ";
-        cout << school_year->data_classes[index].data_student[i].lastname << " ";
-		cout << school_year->data_classes[index].data_student[i].gender << " ";
-		cout << school_year->data_classes[index].data_student[i].birthday << " ";
+		cout << setw(4) << left << school_year->data_classes[index].data_student[i].student_num;
+		cout << setw(12) << left << school_year->data_classes[index].data_student[i].studentID;
+        	cout << setw(15) << left << school_year->data_classes[index].data_student[i].firstname;
+        	cout << setw(15) << left << school_year->data_classes[index].data_student[i].lastname;
+		cout << setw(9) << left << school_year->data_classes[index].data_student[i].gender;
+		cout << setw(14) << left << school_year->data_classes[index].data_student[i].birthday;
 		cout << school_year->data_classes[index].data_student[i].socialID << endl;
 	}
 }
