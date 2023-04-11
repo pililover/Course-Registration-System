d038bdba52871175f5570fdd1466908dde3c6274
pililover <nkanh22@clc.fitus.edu.vn>
2023-04-11 22:42:59 +0700

    [Fixed] Update result

--- Fake_vector_course/Update_student_result.cpp
+++ Fake_vector_course/Update_student_result.cpp
@@ -1,26 +1,38 @@
 #include "Schoolyear_header.h"
 
-void update_student_result(SchoolYear *ssss, string courseid, string stuid)
+void update_student_result(SchoolYear *school_year)
 {
-    Semester a = ssss->data_semester[ssss->current_semester];
-    Course b;
-    for (int i = 0; i < a.course_num; i++)
+    string courseid, stuid;
+    int check = 0;
+    cout << "Enter Student ID: ";
+    cin >> stuid;
+    cout << "Enter CourseID: ";
+    cin >> courseid;
+    Course *course = nullptr;
+    for (int i = 0; i < school_year->data_semester[school_year->current_semester].course_num; i++)
     {
-        if (a.data_course[i].course_id == courseid)
+        if (courseid == school_year->data_semester[school_year->current_semester].data_course[i].course_id)
         {
-            b = a.data_course[i];
+            course = &(school_year->data_semester[school_year->current_semester].data_course[i]);
+            check = 1;
             break;
         }
+    }
+    if (check == 0)
+    {
+        cout << "CourseID does not exist" << endl;
+        return;
     }
     float new_scores = 0;
     cout << "Enter new scores: ";
     cin >> new_scores;
-    for (int i = 0; i < b.capacity_student; i++)
+    for (int i = 0; i < course->size_student; i++)
     {
-        Student c = b.enrolled_student[i];
-        if (c.studentID == stuid)
+        if (stuid == course->enrolled_student[i].studentID)
         {
-            c.score = new_scores;
+            course->enrolled_student[i].score = new_scores;
+            cout << "Update successfully" << endl;
+            return;
         }
     }
 }
