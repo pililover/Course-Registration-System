e8c912573ac632bebb1a7c74fa6a017879494173
pht2004 <phamhoangtien4444@gmail.com>
2023-04-12 00:37:59 +0700

    Update Update_student_result.cpp

--- Fake_vector_course/Update_student_result.cpp
+++ Fake_vector_course/Update_student_result.cpp
@@ -3,36 +3,31 @@
 void update_student_result(SchoolYear *school_year)
 {
     string courseid, stuid;
-    int check = 0;
     cout << "Enter Student ID: ";
     cin >> stuid;
     cout << "Enter CourseID: ";
     cin >> courseid;
     Course *course = nullptr;
+
     for (int i = 0; i < school_year->data_semester[school_year->current_semester].course_num; i++)
-    {
-        if (courseid == school_year->data_semester[school_year->current_semester].data_course[i].course_id)
-        {
+        if (courseid == school_year->data_semester[school_year->current_semester].data_course[i].course_id){
             course = &(school_year->data_semester[school_year->current_semester].data_course[i]);
-            check = 1;
             break;
         }
-    }
-    if (check == 0)
-    {
+
+    if (course== 0){
         cout << "CourseID does not exist" << endl;
         return;
     }
-    float new_scores = 0;
-    cout << "Enter new scores: ";
-    cin >> new_scores;
+    
     for (int i = 0; i < course->size_student; i++)
-    {
-        if (stuid == course->enrolled_student[i].studentID)
-        {
+        if (stuid == course->enrolled_student[i].studentID){
+            float new_scores = 0;
+            cout << "Enter new scores: ";
+            cin >> new_scores;
             course->enrolled_student[i].score = new_scores;
             cout << "Update successfully" << endl;
             return;
         }
-    }
+    cout << "Student want to update score doen't in this course.";
 }
