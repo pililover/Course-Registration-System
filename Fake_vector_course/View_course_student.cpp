11c9c8e88e8af2bab1d1ee28164693fe7dad3999
pililover <nkanh22@clc.fitus.edu.vn>
2023-04-14 22:43:36 +0700

    [Fixed] Layout course/student

--- Fake_vector_course/View_course_student.cpp
+++ Fake_vector_course/View_course_student.cpp
@@ -2,7 +2,7 @@
 
 void view_list_of_course(SchoolYear *school_year)
 {
-    cout << "\t\tLIST OF COURSE IN THIS SEMESTER" << endl;
+    cout << "\t\t\t\tLIST OF COURSE IN THIS SEMESTER" << endl;
     if (school_year->data_semester[school_year->current_semester].course_num == 0)
     {
         cout << "Course not found" << endl;
@@ -10,7 +10,7 @@ void view_list_of_course(SchoolYear *school_year)
     }
     for (int i = 0; i < school_year->data_semester[school_year->current_semester].course_num; i++)
     {
-        cout << i + 1 << setw(5) << ". " << '\t'
+        cout << "\t" << setw(4) << i + 1 << setw(5) << ". " << '\t'
              << setw(25) << school_year->data_semester[school_year->current_semester].data_course[i].course_id
              << setw(35) << school_year->data_semester[school_year->current_semester].data_course[i].course_name << endl;
     }
@@ -18,13 +18,13 @@ void view_list_of_course(SchoolYear *school_year)
 
 void view_student_in_course(SchoolYear *school_year, int cur_course)
 {
-    cout << '\t' << school_year->data_semester[school_year->current_semester].data_course[cur_course].course_id << " - "
+    cout << "\t\t\t\t\t" << school_year->data_semester[school_year->current_semester].data_course[cur_course].course_id << " - "
          << school_year->data_semester[school_year->current_semester].data_course[cur_course].course_name << endl;
     if (school_year->data_semester[school_year->current_semester].data_course[cur_course].size_student != 0)
     {
         for (int j = 0; j < school_year->data_semester[school_year->current_semester].data_course[cur_course].size_student; j++)
         {
-            cout << j + 1 << setw(4) << ". "
+            cout << '\t' << '\t' << setw(4) << j + 1 << setw(4) << ". " << setw(20)
                  << school_year->data_semester[school_year->current_semester].data_course[cur_course].enrolled_student[j].studentID << setw(20)
                  << school_year->data_semester[school_year->current_semester].data_course[cur_course].enrolled_student[j].firstname << setw(20)
                  << school_year->data_semester[school_year->current_semester].data_course[cur_course].enrolled_student[j].lastname << endl;
@@ -52,7 +52,8 @@ void export_student_from_course_to_file(SchoolYear *school_year, string courseid
         {
             for (int j = 0; j < school_year->data_semester[school_year->current_semester].data_course[i].size_student; j++)
             {
-                op << school_year->data_semester[school_year->current_semester].data_course[i].enrolled_student[j].studentID << ","
+                op << school_year->data_semester[school_year->current_semester].data_course[i].enrolled_student[j].student_num << ","
+                   << school_year->data_semester[school_year->current_semester].data_course[i].enrolled_student[j].studentID << ","
                    << school_year->data_semester[school_year->current_semester].data_course[i].enrolled_student[j].firstname << ","
                    << school_year->data_semester[school_year->current_semester].data_course[i].enrolled_student[j].lastname << endl;
             }
