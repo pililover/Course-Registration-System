873501109fa7aae89216e855c37545a85d71464b
pililover <nkanh22@clc.fitus.edu.vn>
2023-04-21 15:19:05 +0700

    Update View_scoreboard_of_a_course.cpp

--- Fake_vector_course/View_scoreboard_of_a_course.cpp
+++ Fake_vector_course/View_scoreboard_of_a_course.cpp
@@ -7,9 +7,9 @@ void view_scoreboard_of_a_course(SchoolYear *school_year)
     Course *course = nullptr;
     cout << "Enter CourseID: ";
     cin >> courseid;
-    cout << setw(40) << setfill(' ') << "SCOREBOARD OF COURSE" << endl;
-    cout << '\t' << "No"
-         << "\t" << setw(15) << "StudentID" << setw(20) << "Firstname" << setw(20) << "Lastname" << setw(10) << "Other" << setw(9) << "Midterm" << setw(9) << "Final" << setw(9) << "Total" << endl;
+    cout << setw(65) << setfill(' ') << "SCOREBOARD OF COURSE" << endl;
+    cout << setw(8) << left << "No"
+         << setw(9) << "StudentID" << setw(18) << "Firstname" << setw(20) << "Lastname" << setw(15) << "Other" << setw(15) << "Midterm" << setw(16) << "Final" << setw(10) << "Total" << endl;
     for (int i = 0; i < school_year->data_semester[school_year->current_semester].course_num; i++)
     {
         if (courseid == school_year->data_semester[school_year->current_semester].data_course[i].course_id)
@@ -26,13 +26,13 @@ void view_scoreboard_of_a_course(SchoolYear *school_year)
     }
     for (int i = 0; i < course->size_student; i++)
     {
-        cout << '\t' << setw(3) << i + 1 << setw(2) << ". " << '\t'
-             << setw(15) << course->enrolled_student[i].studentID << " "
-             << setw(20) << course->enrolled_student[i].firstname << " "
-             << setw(20) << course->enrolled_student[i].lastname << " "
-             << setw(10) << course->enrolled_student[i].other << " "
-             << setw(9) << course->enrolled_student[i].midterm << " "
-             << setw(9) << course->enrolled_student[i].final << " "
-             << setw(9) << course->enrolled_student[i].score << endl;
+        cout << setw(4) << setfill(' ') << setw(3) << left << i + 1 << setw(5) << left << ". "
+             << setw(21) << left << course->enrolled_student[i].studentID
+             << setw(21) << left << course->enrolled_student[i].firstname
+             << setw(22) << left << course->enrolled_student[i].lastname
+             << setw(15) << left << course->enrolled_student[i].other
+             << setw(14) << left << course->enrolled_student[i].midterm
+             << setw(12) << left << course->enrolled_student[i].final
+             << setw(10) << left << course->enrolled_student[i].score << endl;
     }
 }
