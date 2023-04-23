53011c1d458dd8b3171f8ef881bac3f89b7834ca
pililover <nkanh22@clc.fitus.edu.vn>
2023-04-23 10:23:03 +0700

    Update View_course_student.cpp

--- Fake_vector_course/View_course_student.cpp
+++ Fake_vector_course/View_course_student.cpp
@@ -20,6 +20,8 @@ void view_student_in_course(SchoolYear *school_year, int cur_course)
 {
     cout << "\t\t\t\t\t" << school_year->data_semester[school_year->current_semester].data_course[cur_course].course_id << " - "
          << school_year->data_semester[school_year->current_semester].data_course[cur_course].course_name << endl;
+    cout << endl;
+    cout << '\t' << '\t' << setw(4) << "No" << setw(5) << ". " << setw(20) << "StudentID" << setw(20) << "Firstname" << setw(20) << "Lastname" << endl;
     if (school_year->data_semester[school_year->current_semester].data_course[cur_course].size_student != 0)
     {
         for (int j = 0; j < school_year->data_semester[school_year->current_semester].data_course[cur_course].size_student; j++)
@@ -33,6 +35,37 @@ void view_student_in_course(SchoolYear *school_year, int cur_course)
     else
     {
         cout << "Student not found" << endl;
+    }
+}
+
+void view_course_in_schoolyear(System systems, string year_name)
+{
+    if (systems.year_num == 0)
+    {
+        cout << "NO SCHOOL YEAR AVAILABLE" << endl;
+        return;
+    }
+    int check = 0;
+    cout << "\t\t\t\tLIST OF COURSE IN " << year_name << endl;
+    for (int i = 0; i < systems.year_num; i++)
+    {
+        if (systems.data_schoolyear[i].year_name == year_name)
+        {
+            check = 1;
+            for (int j = 0; j < systems.data_schoolyear[i].semester_num; j++)
+            {
+                for (int k = 0; k < systems.data_schoolyear[i].data_semester[j].course_num; k++)
+                {
+                    cout << "\t" << setw(4) << k + 1 << setw(5) << ". " << '\t'
+                         << setw(25) << systems.data_schoolyear[i].data_semester[j].data_course[k].course_id
+                         << setw(35) << systems.data_schoolyear[i].data_semester[j].data_course[k].course_name << endl;
+                }
+            }
+        }
+    }
+    if (check == 0)
+    {
+        cout << "School year not found" << endl;
     }
 }
 
