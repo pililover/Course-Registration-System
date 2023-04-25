e44c043108885149b3482aed63b660ca95a12212
VTKKhoi <118881962+VTKKhoi@users.noreply.github.com>
2023-04-25 23:02:55 +0700

    set_color

--- Fake_vector_course/Log.cpp
+++ Fake_vector_course/Log.cpp
@@ -230,3 +230,19 @@ void change_password(node *accHead, string u_name, string &pass)
         p = p->next;
     }
 }
+
+void set_color(int code)
+{
+    HANDLE color = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(color, code);
+}
+/*Code
+0 = Black       8 = Gray
+1 = Blue        9 = Light Blue
+2 = Green      10 = Light Green
+3 = Aqua       11 = Light Aqua
+4 = Red        12 = Light Red
+5 = Purple     13 = Light Purple
+6 = Yellow     14 = Light Yellow
+7 = White      15 = Bright White
+*/
